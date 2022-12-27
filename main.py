18c1482bae6586ca350aa1ef9bc919b919fe19dc
dodo146 <94894937+dodo146@users.noreply.github.com>
2022-12-27 18:40:00 +0300

    deleted limiter lines

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 import random
 from flask_migrate import Migrate
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from flask_mail import Mail,Message
 import time
 from threading import Thread
